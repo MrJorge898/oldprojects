f75aac8ae78c4e2512cf5692d708c9777586fdef
Jorge <110218372+MrJorge898@users.noreply.github.com>
2025-08-13 16:56:14 -0400

    Add files via upload

---
+++ go.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Vigorous by Mr Draco97 (Only chapter 1 lol)")
+	fmt.Println("First Arc – The Vestiges")
+	fmt.Println("D/Chapter One – Neglect")
+	fmt.Println("-[December 15th, 2000 — 3:00 P.M.]-")
+	fmt.Println("“Well, isn’t it just beautiful today… good ol’ cloud-filled, white sky,” Draco pondered, strolling down the broken, snowed-on pavement taking a walk outside to calm his nerves as he had an exam, taking in the environment for a relaxing nostalgia trip. While Monkey Town wasn’t the nicest town ever since the new government, it did retain some fond memories for Draco. Especially the park.")
+	fmt.Println("Skeletal trees with snow falling from atop the branches, squirrels running along the trunk of the trees, the frozen pond. “College has me all stressed and stuff! But this place never fails to soothe me,” he mused in satisfaction, a small smile appearing on his face as he stretched his arms back. “Guess that's the last good thing about Monkey Town…”")
+	fmt.Println("As he continued his stroll, he looked around the playground. He saw the slides, the seesaw and the broken swings, all of which brought a sense of happiness to him – what stood to him most though, was a little kid. “If only these kids could enjoy the old, not broken down playground,” Draco remarked, sounding somewhat pitiful. But then, he spotted something that rubbed the smile straight off his face. “Wait… why’s that officer with him?”")
+	fmt.Println("I should watch out for that kid, the police always seem to be up to some creepy or dirty stuff, he mused as he sat down on a bench and took out his phone, eavesdropping on their conversation.")
+	fmt.Println("“Ey, how ye doin’?” the officer asked the child.")
+	fmt.Println("“I’m doing fine sir!” the child said, saluting the officer with all the respect of a bright young lad. “Thank you for asking, sir! How are you, sir?”")
+	fmt.Println("“Ah, well I’m fine. Yer a nice kid, I’ll give ye that,” the officer chuckled. “Gotta ask ye though pal, ye seen any o’ them troublemakin’ celestials ‘round ‘ere?”")
+}
